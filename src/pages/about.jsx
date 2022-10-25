76a4348a7472b30591386758008332bda4acd3da
bijink <kbijin528.pro@gmail.com>
2022-10-25 18:44:07 +0530

    part 6

---
+++ src/pages/about.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const about = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>About page</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur
+          quod harum ipsum fuga modi consequuntur. Nesciunt rerum quia ipsa
+          consectetur quam quae quo necessitatibus velit error, sint ex libero
+          provident? Esse natus possimus iste, temporibus suscipit dolorum.
+          Exercitationem sit nisi commodi rerum expedita veritatis, hic in odit
+          cumque minus aliquid?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur
+          quod harum ipsum fuga modi consequuntur. Nesciunt rerum quia ipsa
+          consectetur quam quae quo necessitatibus velit error, sint ex libero
+          provident? Esse natus possimus iste, temporibus suscipit dolorum.
+          Exercitationem sit nisi commodi rerum expedita veritatis, hic in odit
+          cumque minus aliquid?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur
+          quod harum ipsum fuga modi consequuntur. Nesciunt rerum quia ipsa
+          consectetur quam quae quo necessitatibus velit error, sint ex libero
+          provident? Esse natus possimus iste, temporibus suscipit dolorum.
+          Exercitationem sit nisi commodi rerum expedita veritatis, hic in odit
+          cumque minus aliquid?
+        </p>
+      </div>
+    </Layout>
+  )
+}
+export default about
---
+++ src/pages/projects/index.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../../components/Layout"
+import * as styles from "../../styles/projects.module.css"
+
+const project = () => {
+  return (
+    <Layout>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Project & websites I've Created</h3>
+      </div>
+    </Layout>
+  )
+}
+
+export default project
